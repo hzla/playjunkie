00fa7bc164b7ad03c84117a84c7beb64c2214823
hzla00 <andylee.hzl@gmail.com>
2017-03-25 12:23:25 -0700

    remove redis init

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,2 @@
-uri = URI.parse("redis://redistogo:f5e3028ee04243536b0258a914428a91@koi.redistogo.com:11707/" )
-REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+# uri = URI.parse("redis://redistogo:f5e3028ee04243536b0258a914428a91@koi.redistogo.com:11707/" )
+# REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
