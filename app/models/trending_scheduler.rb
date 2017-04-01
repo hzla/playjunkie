eda113e28c8ce733ea34c7452df6fdb477febd3d
hzla00 <andylee.hzl@gmail.com>
2017-04-01 13:16:42 -0700

    update trending scheduler

--- app/models/trending_scheduler.rb
+++ app/models/trending_scheduler.rb
@@ -4,7 +4,8 @@ class TrendingScheduler
   include Sidekiq::Worker
 
   def perform
-  	Quiz.where(is_preview?: true).destroy_all
-    # Quiz.shift_daily_view_counts
+  	# Quiz.where(is_preview?: true).destroy_all
+  	p "shifting daily view counts"
+    Quiz.shift_daily_view_counts
   end
 end
